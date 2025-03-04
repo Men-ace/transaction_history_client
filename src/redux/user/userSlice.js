3ae47e77dbfaf3d16855187825230e5b5709920a
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-04 19:44:15 +1100

    made a s

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,21 +1,30 @@
-// slice - used to define and build initial state and functions to update state 
+// Slice - Used to build and define initial state 
+// and functions(reducer functions) to update state
 
-// 1. define initial state 
-// 2. Define Reducer Functions | Create a slice 
+import { createSlice } from "@reduxjs/toolkit"
 
-const initialState ={
-    user:{
+// 1. Define Initial State
+// 2. Define Reducer Functions | Create a slice
 
-    }
+const initialState = {
+  currentUser: {},
 }
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers:{
-        setUser: (state,action) =>{
-            const { payload } = action 
-            state.user = payload 
-        }
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      const { payload } = action
+      state.currentUser = payload
     }
+  }
 })
+
+// These are all build and given by createSlice function
+// get reducer and rename it to userReducer
+const { reducer: userReducer, actions } = userSlice
+
+export const { setUser } =  actions
+
+export default userReducer
