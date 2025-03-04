cbcffdc91a8a28ea95b7cc7722302d3b4ecac38f
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-04 18:30:36 +1100

    added form and table to page

--- src/pages/TransactionPage.jsx
+++ src/pages/TransactionPage.jsx
@@ -1,17 +1,39 @@
-/* eslint-disable react/prop-types */
+ 
 import { Container } from "react-bootstrap";
 import TopNavbar from "../components/TopNavbar";
 import TransactionForm from "../components/TransactionForm";
+import { useEffect, useState } from "react";
+import TransactionTable from "../components/TransactionTable";
+import { getTransactions } from "../axios/transactionAxios";
+import { useSelector } from "react-redux";
 
-const TransactionPage = (props) => {
-  const { user } = props
+const TransactionPage = () => {
+  const [transactions, setTransactions] = useState([])
+
+  // Get the currentUser from redux store
+  const { currentUser } = useSelector(state => state.user)
+  
+  const fetchTransactions = async() => {
+    const response = await getTransactions(currentUser.id)
+
+    if(response.status === "success"){
+      setTransactions(response.data)
+    }
+  }
+
+  useEffect(()=>{
+    fetchTransactions()
+  }, [])
 
   return ( 
       <Container>
-        <TopNavbar userName={user.name} />
+        <TopNavbar userName={currentUser.name} />
 
         {/* Transaction Form */}
-        <TransactionForm userId={user.id} />
+        <TransactionForm userId={currentUser.id} fetchTransactions={fetchTransactions} />
+
+        {/* Transaction Table */}
+        <TransactionTable transactions={transactions} />
       </Container>
    );
 }
