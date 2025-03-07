e2d350caa593e0736141c654b7062481b4b7e379
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-08 08:46:04 +1100

    added transaction action

---
+++ src/redux/transaction/transactionAction.js
@@ -0,0 +1,34 @@
+import {
+    createTransaction,
+    deleteTransactions,
+    getTransactions,
+  } from "../../axios/transaction/transactionAxios";
+  import { toast } from "react-toastify";
+  import { setTransaction } from "./transactionSlice";
+  
+  export const addTransaction =
+    (transactionObject, userId) => async (dispatch) => {
+      const response = await createTransaction(transactionObject, userId);
+      if (response.status === "error") {
+        return toast.error(response.message);
+      }
+      dispatch(showTransactions(userId));
+      toast.success(response.message);
+    };
+  
+  export const showTransactions = (userId) => async (dispatch) => {
+    const response = await getTransactions(userId);
+    if (response.status === "error") {
+      return toast.error(response.message);
+    }
+    dispatch(setTransaction(response.data));
+  };
+  
+  export const removeTransactions = (selectedIds, userId) => async (dispatch) => {
+    const response = await deleteTransactions(selectedIds, userId);
+    if (response.status === "error") {
+      return toast.error(response.message);
+    }
+    toast.success(response.data.deletedCount + " " + response.message);
+    dispatch(showTransactions(userId));
+  };
