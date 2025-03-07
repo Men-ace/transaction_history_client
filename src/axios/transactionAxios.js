06b2eb59fb0b77a1c3f697364ecb22cc1fe7103b
Shailesh Gharti <124006792+Men-ace@users.noreply.github.com>
2025-03-08 09:00:21 +1100

    aded delete in axios for transaction

--- src/axios/transactionAxios.js
+++ src/axios/transactionAxios.js
@@ -1,29 +1,46 @@
-import axios from "axios"
+import axios from "axios";
 
-// API SERVER URL
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-const transactionEndpoint = '/api/transactions'
+const BASE_URL = import.meta.env.PROD ? "" : import.meta.env.VITE_BASE_API_URL;
+const TRANSACTION_ENDPOINT = "/api/transaction";
 
-const API_URL = API_BASE_URL + transactionEndpoint
+const API_URL = `${BASE_URL}${TRANSACTION_ENDPOINT}`;
 
-// Create transaction | POST
-export const createTransaction = (transactionObj) => {
-  const response = axios.post(`${API_URL}`, transactionObj)
-                    .then((res)=> res.data)
-                    .catch((error) => console.log(error))
+// CREATE transaction | POST
+export const createTransaction = (transactionObject, userId) => {
+  const response = axios
+    .post(`${API_URL}`, transactionObject, {
+      headers: {
+        authorization: userId,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+  return response;
+};
 
-  return response
-}
-
-// Get Transactions | GET
+// INDEX | GET Transactions
 export const getTransactions = (userId) => {
-  const response = axios.get(`${API_URL}`, {
-                    headers: {
-                      Authorization: userId
-                    }
-                    })
-                    .then((res)=> res.data)
-                    .catch((error) => console.log(error))
+  const response = axios
+    .get(`${API_URL}`, {
+      headers: {
+        authorization: userId,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+  return response;
+};
 
-  return response
-}
+// DELETE || Delete Transaction
+export const deleteTransactions = (selectedIds, userId) => {
+  const response = axios
+    .delete(`${API_URL}`, {
+      data: selectedIds,
+      headers: {
+        authorization: userId,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+  return response;
+};
